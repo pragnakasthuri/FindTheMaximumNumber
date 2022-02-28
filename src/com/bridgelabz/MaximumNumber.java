c3ba8abeca1f02872845f1d939d188666d416afe
pragnakasthruri <kpragna25@gmail.com>
2022-02-28 20:50:17 +0530

    Adding the code for TC1.1

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -3,9 +3,9 @@
  */
 package com.bridgelabz;
 
-public class FindTheMaximum {
+public class MaximumNumber{
     public static void main(String[] args) {
-        findMaximum(12, 4, 3);
+        System.out.println("Maximum Number: " +findMaximumNumber(22, 1, 3));
     }
 
     /**
@@ -14,17 +14,17 @@ public class FindTheMaximum {
      * @param number3
      * Used to find the maximum number of given three numbers using compareTo method
      */
-    private static void findMaximum(Integer number1,Integer number2 ,Integer number3) {
+    private static Integer findMaximumNumber(Integer number1, Integer number2 , Integer number3) {
         if((number1.compareTo(number2) == 1) && (number1.compareTo(number3) == 1))
         {
-            System.out.println("number1 is greater than number2 and number3");
+            return number1;
 
         }else if((number2.compareTo(number1) == 1) && (number2.compareTo(number3) == 1))
         {
-            System.out.println("number2 is greater than number1 and number3");
+            return number2;
         }
         else{
-            System.out.println("number3 is greater than number1 and number2");
+            return number3;
         }
     }
 }
