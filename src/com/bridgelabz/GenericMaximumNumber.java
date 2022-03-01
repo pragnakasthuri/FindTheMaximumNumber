235a4006563587378a00396f6be5192767cf5ff1
pragnakasthruri <kpragna25@gmail.com>
2022-03-01 13:28:12 +0530

    Adding the code for generic class

--- src/com/bridgelabz/GenericMaximumNumber.java
+++ src/com/bridgelabz/GenericMaximumNumber.java
@@ -1,34 +1,47 @@
 package com.bridgelabz;
 
 /**
- * Refactor all the 3 to One Generic Method and find the maximum
+ * Refactor to create Generic class to take in 3 variables of generic type
  */
 
-public class GenericMaximumNumber {
+public class GenericMaximumNumber<T extends Comparable, U extends Comparable, V extends Comparable> {
+
+    private T input1;
+    private U input2;
+    private V input3;
+
+    GenericMaximumNumber(T input1, U input2, V input3) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Maximum String: " +findMaximumString("Pineapple", "Apple", "Banana"));
-        System.out.println("Maximum Integer Number: " +findMaximumString(24, 55, 22));
-        System.out.println("Maximum Float Number: " +findMaximumString(23.5f, 55.7f, 99.7f));
+        GenericMaximumNumber<Integer, Integer, Integer> maximumIntegerNumber = new GenericMaximumNumber<Integer, Integer, Integer> (12, 5, 7);
+        maximumIntegerNumber.testMaximum();
+        GenericMaximumNumber<Float, Float, Float> maximumFloatNumber = new GenericMaximumNumber<Float, Float, Float> (12.4f, 55.6f, 7.8f);
+        maximumFloatNumber.testMaximum();
+        GenericMaximumNumber<String, String, String> maximumStringNumber = new GenericMaximumNumber<String, String, String>("Banana", "Apple", "Peach");
+        maximumStringNumber.testMaximum();
+    }
+
+    private void testMaximum() {
+        testMaximum(input1, input2, input3);
     }
 
     /**
-     * @param string1
-     * @param string2
-     * @param string3
      * Used to find the maximum string lexicographically(Dictionary order)
-     * of given three numbers using compareTo method
+     * of given three strings and max number from 3 numbers using compareTo method
      */
-    public static <T extends Comparable> T findMaximumString(T string1, T string2, T string3) {
-        if((string1.compareTo(string2) > 0) && (string1.compareTo(string3) > 0))
+    public static <T extends Comparable, U extends Comparable, V extends Comparable> void testMaximum(T input1, U input2, V input3) {
+        if((input1.compareTo(input2) > 0) && (input1.compareTo(input3) > 0))
         {
-            return string1;
-
-        }else if((string2.compareTo(string1) > 0) && (string2.compareTo(string3) > 0))
+            System.out.println("Maximum: " +input1);
+        } else if((input2.compareTo(input1) > 0) && (input2.compareTo(input3) > 0))
         {
-            return string2;
-        }
-        else{
-            return string3;
+            System.out.println("Maximum: " +input2);
+        } else{
+            System.out.println("Maximum: " +input3);
         }
     }
 }
