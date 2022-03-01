eec2ddf7adc08adbae86b214f81ad430292dd978
pragnakasthruri <kpragna25@gmail.com>
2022-03-01 15:36:26 +0530

    Adding the  code for UC5

--- src/com/bridgelabz/GenericMaximumNumber.java
+++ src/com/bridgelabz/GenericMaximumNumber.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 /**
- * Refactor to create Generic class to take in 3 variables of generic type
+ * Write printMax Generic Method which is internally called from testMaximum
  */
 
 public class GenericMaximumNumber<T extends Comparable, U extends Comparable, V extends Comparable> {
@@ -17,11 +17,11 @@ public class GenericMaximumNumber<T extends Comparable, U extends Comparable, V 
     }
 
     public static void main(String[] args) {
-        GenericMaximumNumber<Integer, Integer, Integer> maximumIntegerNumber = new GenericMaximumNumber<Integer, Integer, Integer> (12, 5, 7);
+        GenericMaximumNumber<Integer, Integer, Integer> maximumIntegerNumber = new GenericMaximumNumber(12, 5, 7);
         maximumIntegerNumber.testMaximum();
-        GenericMaximumNumber<Float, Float, Float> maximumFloatNumber = new GenericMaximumNumber<Float, Float, Float> (12.4f, 55.6f, 7.8f);
+        GenericMaximumNumber<Float, Float, Float> maximumFloatNumber = new GenericMaximumNumber(12.4f, 55.6f, 7.8f);
         maximumFloatNumber.testMaximum();
-        GenericMaximumNumber<String, String, String> maximumStringNumber = new GenericMaximumNumber<String, String, String>("Banana", "Apple", "Peach");
+        GenericMaximumNumber<String, String, String> maximumStringNumber = new GenericMaximumNumber("Banana", "Apple", "Peach");
         maximumStringNumber.testMaximum();
     }
 
@@ -32,16 +32,25 @@ public class GenericMaximumNumber<T extends Comparable, U extends Comparable, V 
     /**
      * Used to find the maximum string lexicographically(Dictionary order)
      * of given three strings and max number from 3 numbers using compareTo method
+     * and call printMax method internally
      */
     public static <T extends Comparable, U extends Comparable, V extends Comparable> void testMaximum(T input1, U input2, V input3) {
         if((input1.compareTo(input2) > 0) && (input1.compareTo(input3) > 0))
         {
-            System.out.println("Maximum: " +input1);
+           printMax(input1);
         } else if((input2.compareTo(input1) > 0) && (input2.compareTo(input3) > 0))
         {
-            System.out.println("Maximum: " +input2);
+            printMax(input2);
         } else{
-            System.out.println("Maximum: " +input3);
+            printMax(input3);
         }
+    }
+
+    /**
+     * @param input
+     * @param <T>
+     */
+    public static <T> void printMax(T input) {
+        System.out.println("Maximum: " + input);
     }
 }
