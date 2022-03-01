48e517250fd852cefdd64716a2854432fe1333a8
pragnakasthruri <kpragna25@gmail.com>
2022-03-01 23:06:26 +0530

    Adding the code for printMax Generic Method which is internally called from testMaximum

--- src/com/bridgelabz/GenericMaximumNumber.java
+++ src/com/bridgelabz/GenericMaximumNumber.java
@@ -1,49 +1,42 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+
 /**
  * Write printMax Generic Method which is internally called from testMaximum
  */
 
-public class GenericMaximumNumber<T extends Comparable, U extends Comparable, V extends Comparable> {
+public class GenericMaximumNumber<T extends Comparable> {
 
-    private T input1;
-    private U input2;
-    private V input3;
+    private T[] input1;
 
-    GenericMaximumNumber(T input1, U input2, V input3) {
-        this.input1 = input1;
-        this.input2 = input2;
-        this.input3 = input3;
+    /**
+     * @param inputParams using var args...
+     */
+    GenericMaximumNumber(T... inputParams) {
+        this.input1 = inputParams;
     }
 
     public static void main(String[] args) {
-        GenericMaximumNumber<Integer, Integer, Integer> maximumIntegerNumber = new GenericMaximumNumber(12, 5, 7);
+        GenericMaximumNumber<Integer> maximumIntegerNumber = new GenericMaximumNumber(12, 5, 7, 55);
         maximumIntegerNumber.testMaximum();
-        GenericMaximumNumber<Float, Float, Float> maximumFloatNumber = new GenericMaximumNumber(12.4f, 55.6f, 7.8f);
+        GenericMaximumNumber<Float> maximumFloatNumber = new GenericMaximumNumber(12.4f, 55.6f, 7.8f, 78.4f);
         maximumFloatNumber.testMaximum();
-        GenericMaximumNumber<String, String, String> maximumStringNumber = new GenericMaximumNumber("Banana", "Apple", "Peach");
+        GenericMaximumNumber<String> maximumStringNumber = new GenericMaximumNumber("Banana", "Apple", "Peach", "Guava", "Pomegranate");
         maximumStringNumber.testMaximum();
     }
 
     private void testMaximum() {
-        testMaximum(input1, input2, input3);
+        testMaximum(input1);
     }
 
     /**
-     * Used to find the maximum string lexicographically(Dictionary order)
-     * of given three strings and max number from 3 numbers using compareTo method
-     * and call printMax method internally
+     * @param input1
+     * @param <T>
      */
-    public static <T extends Comparable, U extends Comparable, V extends Comparable> void testMaximum(T input1, U input2, V input3) {
-        if((input1.compareTo(input2) > 0) && (input1.compareTo(input3) > 0))
-        {
-           printMax(input1);
-        } else if((input2.compareTo(input1) > 0) && (input2.compareTo(input3) > 0))
-        {
-            printMax(input2);
-        } else{
-            printMax(input3);
-        }
+    public static <T extends Comparable> void testMaximum(T... input1) {
+        Arrays.sort(input1);
+        printMax(input1[input1.length-1]);
     }
 
     /**
