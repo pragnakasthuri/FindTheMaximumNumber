3f68a0b108add76320b792a18402874185eac921
pragnakasthruri <kpragna25@gmail.com>
2022-03-01 22:57:59 +0530

    Adding the code for UC4

--- src/com/bridgelabz/GenericMaximumNumber.java
+++ src/com/bridgelabz/GenericMaximumNumber.java
@@ -10,16 +10,19 @@ public class GenericMaximumNumber<T extends Comparable> {
 
     private T[] input1;
 
+    /**
+     * @param inputParams using var args...
+     */
     GenericMaximumNumber(T... inputParams) {
         this.input1 = inputParams;
     }
 
     public static void main(String[] args) {
-        GenericMaximumNumber<Integer> maximumIntegerNumber = new GenericMaximumNumber(12, 5, 7);
+        GenericMaximumNumber<Integer> maximumIntegerNumber = new GenericMaximumNumber(12, 5, 7, 55);
         maximumIntegerNumber.testMaximum();
         GenericMaximumNumber<Float> maximumFloatNumber = new GenericMaximumNumber(12.4f, 55.6f, 7.8f, 78.4f);
         maximumFloatNumber.testMaximum();
-        GenericMaximumNumber<String> maximumStringNumber = new GenericMaximumNumber("Banana", "Apple", "Peach", "Gua", "Pomogranite");
+        GenericMaximumNumber<String> maximumStringNumber = new GenericMaximumNumber("Banana", "Apple", "Peach", "Gua", "Pomegranate");
         maximumStringNumber.testMaximum();
     }
 
@@ -28,20 +31,14 @@ public class GenericMaximumNumber<T extends Comparable> {
     }
 
     /**
-     * Used to find the maximum string lexicographically(Dictionary order)
-     * of given three strings and max number from 3 numbers using compareTo method
-     * and call printMax method internally
+     * @param input1
+     * @param <T>    Performing sorting and printing the maximum
      */
     public static <T extends Comparable> void testMaximum(T... input1) {
         Arrays.sort(input1);
-        printMax(input1[input1.length-1]);
-    }
-
-    /**
-     * @param input
-     * @param <T>
-     */
-    public static <T> void printMax(T input) {
-        System.out.println("Maximum: " + input);
+        System.out.println(("Maximum: " +input1[input1.length - 1]));
     }
 }
+
+
+
